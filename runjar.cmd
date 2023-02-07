bb6ee7bd2b0c2760c699b6322b0298746c9a9de6
dbg14hi <dbg14@hi.is>
2023-02-07 12:09:50 +0000

    finito

---
+++ runjar.cmd
@@ -0,0 +1,1 @@
+java -cp target\dependency\commons-numbers-primes-1.1.jar;target\ass4-1.0-SNAPSHOT.jar is.hi.dbg14.hbv202g.ass4.App
--- src/main/java/is/hi/dbg14/hbv202g/ass4/App.java
+++ src/main/java/is/hi/dbg14/hbv202g/ass4/App.java
@@ -1,13 +1,14 @@
 package is.hi.dbg14.hbv202g.ass4;
 
+import org.apache.commons.numbers.primes.Primes;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int s = Primes.nextPrime(123456789);
+
+        System.out.println(s);
     }
 }
